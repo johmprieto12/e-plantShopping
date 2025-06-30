ab19a0b591e69b5708fb33d7a5317e56257d9cb3
Johm Prieto <johmprieto@gmail.com>
2025-06-30 00:16:17 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -256,11 +256,11 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (product) => {
-        dispatchEvent(addItem(product));
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
 
-        setAddedToCart((prevState) => ({
-            ...prevState,
-            [product.name]: true,
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+            ...prevState, // Spread the previous state to retain existing entries
+            [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
         }));
     };
     
